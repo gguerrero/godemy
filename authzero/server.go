e7a8877824c5a7c424ba9808aa99b534f4b67716
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2019-12-16 15:23:01 +0400

    Convert yet more spaces to tabs

--- authzero/server.go
+++ authzero/server.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"./auth"
-	"./router"
+  "./auth"
+  "./router"
 
-	"net/http"
-	"log"
-	"time"
+  "net/http"
+  "log"
+  "time"
 )
 
 const address string = "localhost:8000"
 
 func main() {
-	authJwtMiddleware := auth.New()
-	routerHandler := router.Handler(authJwtMiddleware)
+  authJwtMiddleware := auth.New()
+  routerHandler := router.Handler(authJwtMiddleware)
 
-	log.Print("Booting Server on " + address + "...")
+  log.Print("Booting Server on " + address + "...")
 
-	srv := &http.Server{
-		Handler: routerHandler,
-		Addr:    address,
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
+  srv := &http.Server{
+  Handler: routerHandler,
+  Addr:    address,
+  // Good practice: enforce timeouts for servers you create!
+  WriteTimeout: 15 * time.Second,
+  ReadTimeout:  15 * time.Second,
+  }
 
-	log.Fatal(srv.ListenAndServe())
+  log.Fatal(srv.ListenAndServe())
 }
