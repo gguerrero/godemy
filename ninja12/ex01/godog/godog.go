acc1165bb10ac2e81cb4eee6966e08ae7ad09680
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2020-01-02 16:54:50 +0400

    Fix Years function return

--- ninja12/ex01/godog/godog.go
+++ ninja12/ex01/godog/godog.go
@@ -5,6 +5,6 @@ const humanAgeRatio int = 7
 // Years functions allows you to receive a human number of ages and will return the equivalent age for a dog.
 //
 // Note: Don't input a negative number or you'll have a problem!
-func Years(humanYears int){
+func Years(humanYears int) int {
   return humanYears * humanAgeRatio
 }
---
+++ ninja12/ex01/godog/godog_test.go
@@ -0,0 +1,9 @@
+package godog
+
+import "fmt"
+
+func ExampleYears() {
+  fmt.Println("A 7 years old dog is equivalent to", Years(7), "years old!")
+  // Output:
+  // A 7 years old dog is equivalent to 49 years old!
+}
